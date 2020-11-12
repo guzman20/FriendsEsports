7a044bba9b86730bf20b3e2ef4f3d8882be881cd
alexret <alex.relano18@gmail.com>
2020-11-12 11:42:46 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/controladores/IndexController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+	@GetMapping("/home")
+	public String  get_home(Model modelo) {
+
+	    return "home";
+	}	
+	
+	
+	@RequestMapping(value="/prueba", method=RequestMethod.POST)
+	public String  get_prueba(Model modelo) {
+
+	    return "prueba";
+	}	
+
+}
