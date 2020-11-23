7c17b22851c950f2805ba33d7a98a8b5756eb98a
alexret <alex.relano18@gmail.com>
2020-11-23 12:52:29 +0100

    Corregido

--- src/main/java/com/example/demo/dao/ProductoDaoImp.java
+++ src/main/java/com/example/demo/dao/ProductoDaoImp.java
@@ -37,10 +37,11 @@ public class ProductoDaoImp extends DaoGenericoImp<Producto> implements Producto
 	}	
 
 	@Override
-	public List<Producto> listarProductos() {
+
+	public List<Producto> listarProductos(Integer maximo) {
 		Query query =  this.em.createQuery("FROM Producto");
 		query.setFirstResult(0);
-		query.setMaxResults(8);
+		query.setMaxResults(maximo);
 		List<Producto> lProducto =  query.getResultList();
 
 		if (lProducto != null) {
