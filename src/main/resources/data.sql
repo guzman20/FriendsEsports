3f44b4ae40213b14303e4496158b48395703c141
alexret <alex.relano18@gmail.com>
2021-01-22 09:21:04 +0100

    Ahora crea el usuario bien pero no logea con esa contraseña

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO usuarios ( apellidos, codigoseguridad, direccion_facturacion, email, fecha_nacimiento, nombre, numerotarjeta, passwordusuario, titulartarjeta) 
+INSERT INTO alejandro.usuarios ( apellidos, codigoseguridad, direccion_facturacion, email, fecha_nacimiento, nombre, numerotarjeta, passwordusuario, titulartarjeta) 
 VALUES ('a', '1', 'a', 'a@a.com', '202-25-9', 'a', '1', '$10$caIr0AKQQXvus5hL2fwo..QB6jux0L2OFNdxRH/CMXxyLEYhbrNBe', 'a');
 
-INSERT INTO rol (id_rol, nombre_rol) VALUES ('1', 'ROL_REGISTRADO');
-INSERT INTO rol (nombre_rol) VALUES ('ROL_ADMIN');
+INSERT INTO alejandro.rol (id_rol, nombre_rol) VALUES ('1', 'ROL_REGISTRADO');
+INSERT INTO alejandro.rol (nombre_rol) VALUES ('ROL_ADMIN');
 
-INSERT INTO usuario_rol (id_usuarios, id_rol) VALUES ('1', '1');
+INSERT INTO alejandro.usuario_rol (id_usuarios, id_rol) VALUES ('1', '1');
