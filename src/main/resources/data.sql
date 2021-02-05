9dddec32faeb88045ee43d6d40120d0bd92468b3
guzman20 <58846501+guzman20@users.noreply.github.com>
2021-02-05 21:02:17 +0100

    Commit tonto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,20 +9,20 @@ INSERT INTO alejandro.rol (id_rol, nombre_rol) VALUES ('2','ROL_ADMIN');
 INSERT INTO alejandro.usuario_rol (id_usuarios, id_rol) VALUES ('1', '2');
 INSERT INTO alejandro.usuario_rol (id_usuarios, id_rol) VALUES ('2', '1');
 
-INSERT INTO alejandro.productos (descripcionproducto,descuento,imagenproducto,nombreproducto,precio) 
-VALUES ('Pimiento Rojo',0,'','Pimiento',1);
-INSERT INTO alejandro.productos (descripcionproducto,descuento,imagenproducto,nombreproducto,precio) 
-VALUES ('Manzana',0,'','fresco',1);
-INSERT INTO alejandro.productos (descripcionproducto,descuento,imagenproducto,nombreproducto,precio) 
-VALUES ('Pera',0,'','fresco',1);
-INSERT INTO alejandro.productos (descripcionproducto,descuento,imagenproducto,nombreproducto,precio) 
-VALUES ('Platano',0,'','fresco',1);
-INSERT INTO alejandro.productos (descripcionproducto,descuento,imagenproducto,nombreproducto,precio) 
-VALUES ('Lechuga',0,'','fresco',1);
-INSERT INTO alejandro.productos (descripcionproducto,descuento,imagenproducto,nombreproducto,precio) 
-VALUES ('Tomate',0,'','fresco',1);
-INSERT INTO alejandro.productos (descripcionproducto,descuento,imagenproducto,nombreproducto,precio) 
-VALUES ('Espinacas',0,'','fresco',1);
+INSERT INTO alejandro.productos (descripcionproducto,descuento,nombreproducto,precio) 
+VALUES ('Pimiento Rojo',0,'Pimiento',1);
+INSERT INTO alejandro.productos (descripcionproducto,descuento,nombreproducto,precio) 
+VALUES ('Manzana',0,'fresco',1);
+INSERT INTO alejandro.productos (descripcionproducto,descuento,nombreproducto,precio) 
+VALUES ('Pera',0,'fresco',1);
+INSERT INTO alejandro.productos (descripcionproducto,descuento,nombreproducto,precio) 
+VALUES ('Platano',0,'fresco',1);
+INSERT INTO alejandro.productos (descripcionproducto,descuento,nombreproducto,precio) 
+VALUES ('Lechuga',0,'fresco',1);
+INSERT INTO alejandro.productos (descripcionproducto,descuento,nombreproducto,precio) 
+VALUES ('Tomate',0,'fresco',1);
+INSERT INTO alejandro.productos (descripcionproducto,descuento,nombreproducto,precio) 
+VALUES ('Espinacas',0,'fresco',1);
 
 INSERT INTO alejandro.preguntas (pregunta,idproductos,id_usuarios) VALUES ('Esta bueno?','1','1');
 INSERT INTO alejandro.respuestas (respuesta,id_pregunta,id_usuarios) VALUES ('Si','1','2');
